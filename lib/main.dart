203ffe6b41213f837cf451988ab16162bcd9237c
alifaraji <alifaraji65452@gmail.com>
2021-03-20 01:28:26 +0330

    crypto app started

--- lib/main.dart
+++ lib/main.dart
@@ -1,62 +1,19 @@
-/* **************************************
-                 * START*
-************************************** */
-
-import 'package:flutter/cupertino.dart';
+//import 'package:firebase_core/firebase_core.dart';
 import 'package:flutter/material.dart';
-import 'dart:math';
+import 'ui/authentication.dart';
 
-void main() {
-  runApp(LAWidget());
+void main() async {
+  WidgetsFlutterBinding.ensureInitialized();
+  //await Firebase.initializeApp();
+  runApp(MyApp());
 }
 
-class LAWidget extends StatelessWidget {
+class MyApp extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
-      home: Scaffold(body: LHERO()),
-      debugShowCheckedModeBanner: false,
-      theme: ThemeData(
-        primarySwatch: Colors.blue,
-      ),
-    );
-  }
-}
-
-class LHERO extends StatefulWidget {
-  @override
-  _LHEROState createState() => _LHEROState();
-}
-
-class _LHEROState extends State<LHERO> {
-  List<Widget> list = [];
-  Widget createWidget() {
-    print(Random().nextInt(10));
-    list.add(new Icon(Icons.ac_unit_sharp,
-        color: Color.fromARGB(255, Random().nextInt(255), Random().nextInt(255),
-            Random().nextInt(255))));
-    isClicked = true;
-    return new Row(children: list);
-  }
-
-  bool isClicked = false;
-
-  @override
-  Widget build(BuildContext context) {
-    return Row(
-      children: [
-        IconButton(
-            icon: Icon(Icons.point_of_sale),
-            color: Colors.blue,
-            onPressed: () {
-              setState(() {
-                createWidget();
-              });
-            }),
-        Row(
-          children: list,
-        )
-      ],
+      title: 'Crypto Wallet',
+      home: Authentication(),
     );
   }
 }
